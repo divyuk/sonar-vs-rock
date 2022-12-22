d34290029c3b4b81e12507fa0a70b6fd23764247
divyuk <div14kes@gmail.com>
2022-12-22 17:42:19 +0530

    The training pipeline config

--- sonar/constant/training_pipeline/__init__.py
+++ sonar/constant/training_pipeline/__init__.py
@@ -3,10 +3,11 @@ import os
 PIPELINE_NAME: str = "sonar"
 ARTIFACT_DIR: str = "artifact"
 FILE_NAME: str = "sonar.csv"
-SCHEMA_FILE_PATH = os.path.join("config", "schema.yml")
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 SCHEMA_DROP_COLS = "drop_columns"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -15,3 +16,13 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
