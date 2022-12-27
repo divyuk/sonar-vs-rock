370793715acb4cc1b2437aede766c3eb43240fa9
divyuk <div14kes@gmail.com>
2022-12-27 12:33:03 +0530

    Basic Structure refined

--- sonar/components/model_trainer.py
+++ sonar/components/model_trainer.py
@@ -3,6 +3,9 @@ class ModelTrainer:
     def __init__(self) -> None:
         pass
     
+    def train_model(self):
+        pass
+    
     def initiate_model_trainer(self):
         pass
         
