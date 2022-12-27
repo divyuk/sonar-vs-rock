5e4003b7aea326e1aa080a89f0b5672670065ace
divyuk <div14kes@gmail.com>
2022-12-27 12:56:20 +0530

    load numpy array in mem

--- sonar/utils/main_utils.py
+++ sonar/utils/main_utils.py
@@ -46,3 +46,15 @@ def save_object(file_path: str, obj: object) -> None:
         logging.info("Exited the save_object method of MainUtils class")
     except Exception as e:
         raise SonarException(e, sys) from e
+    
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise SonarException(e, sys) from e
