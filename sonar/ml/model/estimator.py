7840ce413c76f7e23d7db46a6b2d7b85d6798fa8
divyuk <div14kes@gmail.com>
2023-01-02 15:53:28 +0530

    Model resolver get best model and check if model exist

--- sonar/ml/model/estimator.py
+++ sonar/ml/model/estimator.py
@@ -28,3 +28,45 @@ class SonarModel:
             return y_hat
         except Exception as e:
             raise 
+
+class ModelResolver:
+    """
+    This will give the latest timestamp model
+    """
+    
+    def __init__(self,model_dir=SAVED_MODEL_DIR):
+        try:
+            self.model_dir = model_dir
+
+        except Exception as e:
+            raise e
+        
+    def get_best_model_path(self,)->str:
+        try:
+            timestamps = list(map(int,os.listdir(self.model_dir)))
+            latest_timestamp = max(timestamps)
+            latest_model_path= os.path.join(self.model_dir,f"{latest_timestamp}",MODEL_FILE_NAME)
+            return latest_model_path
+        except Exception as e:
+            raise e
+
+    def is_model_exists(self)->bool:
+        """
+        This do a preliminary check if model exists and if it exists it returns true
+        """
+        try:
+            if not os.path.exists(self.model_dir):
+                return False
+
+            timestamps = os.listdir(self.model_dir)
+            if len(timestamps)==0:
+                return False
+            
+            latest_model_path = self.get_best_model_path()
+
+            if not os.path.exists(latest_model_path):
+                return False
+
+            return True
+        except Exception as e:
+            raise 
